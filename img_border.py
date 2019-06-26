0c2ad3fcda5e55e6692515adffe2ad03c5e4a5d3
royarita <royaritraofficial@gmail.com>
2019-06-26 20:36:04 +0530

    created image border functionalities in open cv

---
+++ img_border.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 23:34:10 2019
+
+@author: aritra
+"""
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+BLUE = [255,0,0]
+
+img1 = cv2.imread('/home/aritra/python_opencv3/database/4.1.04.tiff')
+
+replicate = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REPLICATE)
+reflect = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT)
+reflect101 = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT_101)
+wrap = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_WRAP)
+constant= cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_CONSTANT,value=BLUE)
+
+plt.subplot(231),plt.imshow(img1,'gray'),plt.title('ORIGINAL')
+plt.subplot(232),plt.imshow(replicate,'gray'),plt.title('REPLICATE')
+plt.subplot(233),plt.imshow(reflect,'gray'),plt.title('REFLECT')
+plt.subplot(234),plt.imshow(reflect101,'gray'),plt.title('REFLECT_101')
+plt.subplot(235),plt.imshow(wrap,'gray'),plt.title('WRAP')
+plt.subplot(236),plt.imshow(constant,'gray'),plt.title('CONSTANT')
+
+plt.show()
