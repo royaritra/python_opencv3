2cabf782a23b5a4134527338468042884399e87e
royarita <royaritraofficial@gmail.com>
2019-07-01 21:14:39 +0530

    added different adaptive thresholding techniques

--- adaptive_threshold.py
+++ adaptive_threshold.py
@@ -10,4 +10,16 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 img = cv2.imread('/home/aritra/python_opencv3/database/sudoku-original.jpg',0)
+img1=cv2.medianBlur(img,5)
+th1=cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+th2=cv2.adaptiveThreshold(img1,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
 
+while(True):
+    cv2.imshow('original',img)
+    cv2.imshow('blurred',img1)
+    cv2.imshow('Adaptive Mean',th1)
+    cv2.imshow('Gaussian',th2)
+    k=cv2.waitKey(5)
+    if(k==27):
+        break
+cv2.destroyAllWindows()
